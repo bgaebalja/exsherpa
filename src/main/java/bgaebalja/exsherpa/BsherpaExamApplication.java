2cb06c938f37f11d837d40a4cc2c2d794ae940fd
Sung Hyobin <hellmir610@gmail.com>
2024-11-02 18:04:23 +0900

    Initial commit (#1)
    - 빌드 설정
        - gradle 설정 추가
    
    - 실행 환경 설정
        - production application 설정 추가
        - production application 클래스 추가
        - test application 설정 추가
    
    - 형상 관리
        - .gitignore 추가

---
+++ src/main/java/bgaebalja/exsherpa/BsherpaExamApplication.java
@@ -0,0 +1,11 @@
+package bgaebalja.exsherpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsherpaExamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BsherpaExamApplication.class, args);
+    }
+}
