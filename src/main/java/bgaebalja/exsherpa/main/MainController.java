eafde649459cf96b4fdcac06dcabf6ec31098b1c
Sung Hyobin <hellmir610@gmail.com>
2024-11-02 21:47:16 +0900

    fix(main): 배포 서버에서 메인 페이지 렌더링 엔드포인트를 찾지 못하는 문제 해결 (#11)
    - 메인페이지를 렌더링하는 엔드포인트의 반환값을 main.jsp로 변경

--- src/main/java/bgaebalja/exsherpa/main/MainController.java
+++ src/main/java/bgaebalja/exsherpa/main/MainController.java
@@ -2,12 +2,11 @@ package bgaebalja.exsherpa.main;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
     @GetMapping("/")
     public String getMainPage() {
-        return "main";
+        return "main.jsp";
     }
 }
