0059c4ad028c7f9d6d38798a53080567263b10a7
heosangbeom <luv_3_u@naver.com>
2024-11-03 21:36:24 +0900

    feat(user): 유저 도메인 및 로그인 어노테이션 기능 추가
    - 유저 도메인
    - 로그인 어노테이션

---
+++ src/main/java/bgaebalja/exsherpa/user/annotation/LoginUserArgumentResolver.java
@@ -0,0 +1,35 @@
+package bgaebalja.exsherpa.user.annotation;
+
+import bgaebalja.exsherpa.user.domain.Users;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Slf4j
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+  @Override
+  public boolean supportsParameter(MethodParameter parameter) {
+    boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);
+    boolean hasUsersType = Users.class.isAssignableFrom(parameter.getParameterType());
+    return hasLoginAnnotation && hasUsersType;
+  }
+
+  @Override
+  public Object resolveArgument(
+      MethodParameter parameter, ModelAndViewContainer mavContainer,
+      NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+    HttpServletRequest request =  webRequest.getNativeRequest(HttpServletRequest.class);
+    assert request != null;
+    HttpSession session = request.getSession(false);
+    if (session == null) {
+      return null;
+    }
+    return session.getAttribute("loginUser");
+  }
+}
