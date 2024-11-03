b85b21d8d182509df5541a9fa8be65f5f76f8284
Sung Hyobin <hellmir610@gmail.com>
2024-11-03 15:03:45 +0900

    feat(utility): 포맷 검증을 위한 FormatValidator 클래스와 포맷 변환을 위한 FormatConverter 클래스 추가 (#29)
    - ID 전송값 검증
    - 포맷 검증
    - 포맷 변환

--- src/main/java/bgaebalja/exsherpa/exception/ExceptionMessage.java
+++ src/main/java/bgaebalja/exsherpa/exception/ExceptionMessage.java
@@ -13,10 +13,6 @@ public class ExceptionMessage {
             = "시간값만 LocalDateTime 타입으로 변환할 수 있습니다. 현재 변환 대상 값: %s";
     public static final String PARSING_BOOLEAN_EXCEPTION_MESSAGE
             = "논리형 값만 Boolean 타입으로 변환할 수 있습니다. 현재 변환 대상 값: %s";
-    public static final String INVALID_TARGET_TYPE_EXCEPTION_MESSAGE
-            = "존재하는 도메인만 TargetType으로 변환할 수 있습니다. 현재 변환 대상 값: %s";
-
-    public static final String TOKEN_NO_VALUE_EXCEPTION_MESSAGE = "로그인 후 다시 시도해 주세요.";
 
     public static final String ID_NO_VALUE_EXCEPTION_MESSAGE = "ID는 필수값입니다.";
     public static final String INVALID_ID_EXCEPTION_MESSAGE = "ID는 양의 정수(1 이상의 숫자값)여야 합니다. 전송된 ID: %s";
